8294ef88140de736e87ba921a7f8bfbfe40dfa9d
akonkoo <akonkoo@hotmail.com>
2016-06-12 02:02:34 +0800

    add heroku

--- config.py
+++ config.py
@@ -59,6 +59,18 @@ class ProductionConfig(Config):
 class TestingConfig(Config):
 	TESTING = True
 
+
+class HerokuConfig(ProductionConfig):
+	@classmethod
+	def init_app(cls, app):
+		ProductionConfig.init_app()
+
+		import logging
+		from logging import StreamHandler
+		file_handler = StreamHandler()
+		file_handler.setLevel(logging.WARNING)
+		app.logger.addHandler(file_handler)
+
 config = {
 	'development': DevelopmentConfig,
 	'production': ProductionConfig,
