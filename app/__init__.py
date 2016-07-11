2b85c27185c7a5f500166dbe47de95f88f9b3427
akonkoo <akonkoo@hotmail.com>
2016-07-12 01:38:51 +0800

    add admin

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask.ext.moment import Moment
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
 from flask.ext.pagedown import PageDown
+from flask.ext.admin import Admin
 from config import config
 
 bootstrap = Bootstrap()
@@ -12,6 +13,7 @@ mail = Mail()
 moment = Moment()
 db = SQLAlchemy()
 pagedown = PageDown()
+admin = Admin()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
@@ -30,6 +32,7 @@ def create_app(config_name):
 	db.init_app(app)
 	login_manager.init_app(app)
 	pagedown.init_app(app)
+	admin.init_app(app)
 
 	from .main import main as main_blueprint
 	app.register_blueprint(main_blueprint)
